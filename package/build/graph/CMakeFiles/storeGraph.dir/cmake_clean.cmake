f619b7f3316a51327857f8640999c8c93874fdc7
JunqiaoQiu <junqiqiu@cityu.edu.hk>
2024-04-16 11:26:21 +0800

    adding graph data and fix bugs

---
+++ package/build/graph/CMakeFiles/storeGraph.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/storeGraph.dir/graph.cpp.o"
+  "CMakeFiles/storeGraph.dir/storeGraph.cpp.o"
+  "storeGraph"
+  "storeGraph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/storeGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
